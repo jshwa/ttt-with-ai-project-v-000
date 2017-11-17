a5af04d6f7b53c53f15078958760e9b7746b23e1
Joshua Palacios <palacios.joshua@gmail.com>
2017-11-17 22:37:18 +0000

    automatically backed up by learn

--- lib/game.rb
+++ lib/game.rb
@@ -3,15 +3,6 @@ require 'pry'
 class Game
   include Rules
   attr_accessor :board, :player_1, :player_2
-  # WIN_COMBINATIONS = [
-  #   [0,1,2],
-  #   [3,4,5],
-  #   [6,7,8],
-  #   [0,3,6],
-  #   [1,4,7],
-  #   [2,5,8],
-  #   [0,4,8],
-  #   [6,4,2]]
 
   def initialize(player_1 = Players::Human.new("X"), player_2 = Players::Human.new("O"), board = Board.new)
     @player_1 = player_1
@@ -27,16 +18,9 @@ class Game
     draw? || won?
   end
 
-  # def won?
-  #   WIN_COMBINATIONS.detect do |comb|
-  #     array = [board.cells[comb[0]], board.cells[comb[1]], board.cells[comb[2]]]
-  #     array == ["X", "X", "X"] || array == ["O", "O", "O"]
-  #   end
+  # def draw?
+  #   true if board.full? && !won?
   # end
-
-  def draw?
-    true if board.full? && !won?
-  end
 
   def winner
     board.cells[won?[0]] if won?
